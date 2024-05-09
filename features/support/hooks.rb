05750edb982b6ac912e05fdc428f94b701f2cf36
weleyam <weleyam@gmail.com>
2024-05-09 12:30:03 +0700

    remove generate report

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,11 +2,11 @@ Before do |scenario|
     page.windows[0].maximize
 end
 
-After do |scenario|
-    sleep 3
-    take_screenshot(scenario)
-    Capybara.current_session.driver.quit
-end
+# After do |scenario|
+#     sleep 3
+#     take_screenshot(scenario)
+#     Capybara.current_session.driver.quit
+# end
 
 # AfterStep do |scenario|
 #     sleep 3
@@ -15,14 +15,14 @@ end
 # end
 
 at_exit do
-    ReportBuilder.configure do |config|
-      config.input_path = 'features/support/reports/result-cucumber.json'
-      config.report_path = 'features/support/reports/test_result'
-      config.report_types = [:html]
-      config.report_title = "Test Result"
-      config.includes_image = true
-    end
-    ReportBuilder.build_report
+    # ReportBuilder.configure do |config|
+    #   config.input_path = 'features/support/reports/result-cucumber.json'
+    #   config.report_path = 'features/support/reports/test_result'
+    #   config.report_types = [:html]
+    #   config.report_title = "Test Result"
+    #   config.includes_image = true
+    # end
+    # ReportBuilder.build_report
 
     # Kernel.exit(0)
 end
